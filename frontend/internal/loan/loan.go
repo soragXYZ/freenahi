d6d84a85f9d574843d0ba171c2ff54e475d3171d
soragXYZ <sorag.eth@protonmail.com>
2025-05-15 18:21:27 +0200

    prepare for loan screen

--- frontend/internal/loan/loan.go
+++ frontend/internal/loan/loan.go
@@ -1,20 +1,110 @@
 package loan
 
 import (
-	"image/color"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"freenahiFront/internal/helper"
+	"freenahiFront/internal/settings"
 
 	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/lang"
+	"fyne.io/fyne/v2/widget"
 )
+
+type Loan struct {
+	Loan_account_id      int     `json:"-"` // absent in base data, field added for simplicity
+	Total_amount         float32 `json:"total_amount"`
+	Available_amount     float32 `json:"available_amount"`
+	Used_amount          float32 `json:"used_amount"`
+	Subscription_date    string  `json:"subscription_date"`
+	Maturity_date        string  `json:"maturity_date"`
+	Start_repayment_date string  `json:"start_repayment_date"`
+	Deferred             bool    `json:"deferred"`
+	Next_payment_amount  float32 `json:"next_payment_amount"`
+	Next_payment_date    string  `json:"next_payment_date"`
+	Rate                 float32 `json:"rate"`
+	Nb_payments_left     uint    `json:"nb_payments_left"`
+	Nb_payments_done     uint    `json:"nb_payments_done"`
+	Nb_payments_total    uint    `json:"nb_payments_total"`
+	Last_payment_amount  float32 `json:"last_payment_amount"`
+	Last_payment_date    string  `json:"last_payment_date"`
+	Account_label        string  `json:"account_label"`
+	Insurance_label      string  `json:"insurance_label"`
+	Insurance_amount     float32 `json:"insurance_amount"`
+	Insurance_rate       float32 `json:"insurance_rate"`
+	Duration             uint    `json:"duration"`
+	Loan_type            string  `json:"type"`
+}
 
 func NewLoanScreen(app fyne.App, win fyne.Window) *fyne.Container {
 
-	label := canvas.NewText("Work in progress", color.White)
-	label.TextSize = 50
-	label.TextStyle.Bold = true
-	label.TextStyle.Italic = true
+	loanTable := createLoanTable(app)
 
-	return container.NewVBox(layout.NewSpacer(), container.NewHBox(layout.NewSpacer(), label, layout.NewSpacer()), layout.NewSpacer())
+	return loanTable
+}
+
+// Create the table of of loan
+func createLoanTable(app fyne.App) *fyne.Container {
+
+	loans := getLoans(app)
+	fmt.Println(loans)
+
+	subscriptionDateLabel := widget.NewLabel(lang.L("Subscription date"))
+	subscriptionDateLabel.Alignment = fyne.TextAlignCenter
+
+	valueHeaderLabel := widget.NewLabel(lang.L("Value"))
+	valueHeaderLabel.Alignment = fyne.TextAlignCenter
+
+	durationHeaderLabel := widget.NewLabel(lang.L("Duration"))
+	durationHeaderLabel.Alignment = fyne.TextAlignCenter
+
+	headerContainer := container.NewGridWithColumns(
+		3,
+		subscriptionDateLabel,
+		valueHeaderLabel,
+		durationHeaderLabel,
+	)
+
+	// Faire liste simple
+	// Fenetre qui s ouvre avec details
+	// credit numero | date souscription | type | montant | durée | taux | mensualité | mensualité restantes | montant assurance
+
+	mainContainer := container.NewVBox(headerContainer)
+
+	return mainContainer
+}
+
+// ToDo: modify the function to return an error and display it if sth went wrong in the backend
+// Call the backend endpoint "/loan" and retrieve loans
+func getLoans(app fyne.App) []Loan {
+
+	backendIp := app.Preferences().StringWithFallback(settings.PreferenceBackendIP, settings.BackendIPDefault)
+	backendProtocol := app.Preferences().StringWithFallback(settings.PreferenceBackendProtocol, settings.BackendProtocolDefault)
+	backendPort := app.Preferences().StringWithFallback(settings.PreferenceBackendPort, settings.BackendPortDefault)
+
+	url := fmt.Sprintf("%s://%s:%s/loan/", backendProtocol, backendIp, backendPort)
+	resp, err := http.Get(url)
+	if err != nil {
+		helper.Logger.Error().Err(err).Msg("Cannot run http get request")
+		return nil
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		helper.Logger.Error().Err(err).Msg("ReadAll error")
+		return nil
+	}
+
+	var loans []Loan
+	if err := json.Unmarshal(body, &loans); err != nil {
+		helper.Logger.Error().Err(err).Msg("Cannot unmarshal loans")
+		return nil
+
+	}
+
+	return loans
 }
